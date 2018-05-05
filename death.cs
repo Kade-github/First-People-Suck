eebf2ee8b5f23fe6cfde84fceffed4222f9452c2
Kade <KadePcGames@gmail.com>
2018-05-05 14:53:23 -0700

    New file

---
+++ death.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class death : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			other.transform.position = new Vector3 (-2.96f, 7.21f, 8.68f);
+		}
+	}
+}
